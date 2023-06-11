b84456f72589dd5f002bfa1390679f6b43957e6a
BSaucing <inertiawatchclub@gmail.com>
2023-06-11 09:52:42 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x2E8e257Eaafc128ac21577ed455D91A2209E2c8A"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
